e8d88c1d129ffd434c29fae8b079a39bd88f1546
Rory Murray <rory.christian.murray@gmail.com>
2019-08-07 17:06:46 -0400

    Create mammals in Main

--- src/animalApp/Main.java
+++ src/animalApp/Main.java
@@ -4,6 +4,13 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
+        Mammal panda = new Mammal("Panda", 1869);
+        Mammal zebra = new Mammal("Zebra", 1778);
+        Mammal koala = new Mammal("Koala", 1816);
+        Mammal sloth = new Mammal("Panda", 1804);
+        Mammal armadillo = new Mammal("Armadillo", 1758);
+        Mammal raccoon = new Mammal("Raccoon", 1758);
+        Mammal bigfoot = new Mammal("Bigfoot", 2021);
 
     }
 }
